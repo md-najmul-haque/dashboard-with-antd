95c504da063af6196993cdf6c0a78ecec026763a
Md Najmul Haque <najmulhaque008@gmail.com>
2022-09-20 15:23:55 +0600

    dashboard style changed

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,11 +1,60 @@
+import { LaptopOutlined, NotificationOutlined, UserOutlined } from '@ant-design/icons';
+import { Layout, Menu } from 'antd';
 import React from 'react';
+import './Dashboard.css'
 
-const Dashboard = () => {
-    return (
-        <div>
-            <h2>welcome to Dashboard</h2>
-        </div>
-    );
-};
+const { Content, Sider } = Layout;
+const items1 = ['1', '2'].map((key) => ({
+    key,
+    label: `nav ${key}`,
+}));
+const items2 = [UserOutlined, LaptopOutlined, NotificationOutlined].map((icon, index) => {
+    const key = String(index + 1);
+    return {
+        key: `sub${key}`,
+        icon: React.createElement(icon),
+        label: `subnav ${key}`,
+        children: new Array(4).fill(null).map((_, j) => {
+            const subKey = index * 4 + j + 1;
+            return {
+                key: subKey,
+                label: `option${subKey}`,
+            };
+        }),
+    };
+});
+
+const Dashboard = () => (
+    <Layout>
+        <Content>
+            <Layout
+                className="site-layout-background"
+                style={{
+                    padding: '24px 0',
+                }}
+            >
+                <Sider className="site-layout-background side-bar">
+                    <Menu
+                        mode="inline"
+                        defaultSelectedKeys={['1']}
+                        defaultOpenKeys={['sub1']}
+                        style={{
+                            height: '100%',
+                        }}
+                        items={items2}
+                    />
+                </Sider>
+                <Content
+                    style={{
+                        padding: '0 24px',
+                        minHeight: 280,
+                    }}
+                >
+                    Content
+                </Content>
+            </Layout>
+        </Content>
+    </Layout>
+);
 
 export default Dashboard;
--- src/components/Shared/Navbar/Navbar.js
+++ src/components/Shared/Navbar/Navbar.js
@@ -6,6 +6,7 @@ import logo from "../../../assets/logo.png"
 import { useAuthState } from 'react-firebase-hooks/auth';
 import auth from '../../../firebase.init';
 import { UserOutlined, BellOutlined } from '@ant-design/icons';
+import './Navbar.css'
 
 const Navbar = () => {
     const [current, setCurrent] = useState('mail');
@@ -13,43 +14,65 @@ const Navbar = () => {
 
     const onSearch = (value) => console.log(value);
 
-    const items = [
-        {
-            label: (<img
-                src={logo}
-                style={{
-                    width: 100,
-                }}
-                alt=''
-            />),
-            Key: '1'
-        }, {
-            label: (<Search
-                placeholder="input search text"
-                onSearch={onSearch}
-                style={{
-                    width: 200,
-                    marginTop: 5
-                }}
-            />),
-            Key: '2'
-        },
-        {
-            label: (user ? <> <BellOutlined />  </> : ''),
-            key: '3',
-        },
-        {
-            label: (user ? <> <UserOutlined /> <span>{user?.displayName}</span> </> : <Link to="/register">Get Started</Link>),
-            key: '4',
-        }
-    ];
+    // const items = [
+    //     {
+    //         label: (<img
+    //             src={logo}
+    //             style={{
+    //                 width: 100,
+    //             }}
+    //             alt=''
+    //         />),
+    //         Key: '1'
+    //     }, {
+    //         label: (<Search
+    //             placeholder="input search text"
+    //             onSearch={onSearch}
+    //             style={{
+    //                 width: 200,
+    //                 marginTop: 5
+    //             }}
+    //         />),
+    //         Key: '2'
+    //     },
+    //     {
+    //         label: (user ? <> <BellOutlined />  </> : ''),
+    //         key: '3',
+    //     },
+    //     {
+    //         label: (user ? <> <UserOutlined /> <span>{user?.displayName}</span> </> : <Link to="/register">Get Started</Link>),
+    //         key: '4',
+    //     }
+    // ];
 
     const onClick = e => {
         console.log('click ', e);
         setCurrent(e.key);
     };
 
-    return <Menu onClick={onClick} selectedKeys={[current]} mode="horizontal" items={items} />;
+    return (
+        <>
+            <header className='header-area'>
+                <div className='logo'>
+                    <img src={logo} alt="logo" />
+                </div>
+                <div className='search-bar'>
+                    <Search
+                        placeholder="input search text"
+                        onSearch={onSearch}
+                        style={{
+                            width: 300,
+                            marginTop: 5
+                        }}
+                    />
+                </div>
+                <div className='header-right'>
+                    <BellOutlined />
+                    <span className='display-user'>{user ? <> <UserOutlined /> <span>{user?.displayName}</span> </> : <Link to="/register">Get Started</Link>}</span>
+                </div>
+            </header>
+        </>
+    );
 };
 
 export default Navbar;
