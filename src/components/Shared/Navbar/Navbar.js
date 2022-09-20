4990af7c6d0ae63ff65840c7d1cfe07a881863dc
Md Najmul Haque <najmulhaque008@gmail.com>
2022-09-20 12:18:57 +0600

    search field and logo added

--- src/components/Shared/Navbar/Navbar.js
+++ src/components/Shared/Navbar/Navbar.js
@@ -1,15 +1,40 @@
 import { Menu } from 'antd';
+import Search from 'antd/lib/input/Search';
 import React, { useState } from 'react';
 import { Link } from "react-router-dom";
+import logo from "../../../assets/logo.png"
+
+
+const onSearch = (value) => console.log(value);
 
 const items = [
     {
+        label: (<img
+            src={logo}
+            style={{
+                width: 100,
+            }}
+            alt=''
+        />),
+        Key: '0'
+    }, {
+        label: (<Search
+            placeholder="input search text"
+            onSearch={onSearch}
+            style={{
+                width: 200,
+                marginTop: 5
+            }}
+        />),
+        Key: '1'
+    },
+    {
         label: (<Link to="/">Home</Link>),
-        key: '1',
+        key: '2',
     },
     {
         label: (<Link to="/dashboard">Dashboard</Link>),
-        key: '2',
+        key: '3',
     },
     {
         label: (<Link to="/register">Get Started</Link>),
